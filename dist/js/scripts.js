8cfba3c2cfd1aa996eaf78ce1e9da9fb6db42860
Elana Korngold <elana.korngold@gmail.com>
2021-06-24 21:51:30 -0400

    fixed button

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -13,16 +13,35 @@ function clickHandlers(event) {
     .then((data) => showData(data.results));
 }
 
+// function showData(stories) {
+//   var looped = "";
+//   for (let story of stories) {
+//     looped += `
+//         <div class="item">
+//           <h3>${story.title}</h3>
+//           <p>${story.abstract}</p>
+//         </div>
+//         `;
+//   }
+
+//   document.querySelector(".stories").innerHTML = looped;
+// }
+
 function showData(stories) {
-  var looped = "";
-  for (let story of stories) {
-    looped += `
-        <div class="item">
-          <h3>${story.title}</h3>
-          <p>${story.abstract}</p>
-        </div>
-        `;
-  }
+  var looped = stories
+    .map(
+      (result) => `
+      <div class="item">
+        <h3>${result.title}</h3>
+        <p>${result.abstract}</p>
+      </div>
+    `
+    )
+    .join("");
 
   document.querySelector(".stories").innerHTML = looped;
 }
+
+/*
+  add:  author, an image, caption */
+/*latest notes in Jamstack-classone */
